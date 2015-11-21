1315f40025f273cfb1e3959a8233969205e91d67
Deniss Kaibagarovs <oneander@gmail.com>
2015-11-21 10:10:42 +0200

    Setup!

---
+++ Tidy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tidy
+//
+//  Created by Deniss Kaibagarovs on 21/11/15.
+//  Copyright © 2015 Deniss Kaibagarovs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
